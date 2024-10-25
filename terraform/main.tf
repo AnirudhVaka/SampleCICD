43787c03996f74be2ee37d8027829ffb8d7c3a70
Anirudh\anirudh <anirudh@snovasys.com>
2024-10-25 11:19:00 +0530

    Update Main file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,32 @@
 resource "aws_s3_bucket" "website_bucket" {
-  bucket = "samplewebvaultbucket"  # Replace with a unique name
-  acl    = "public-read"
+  bucket = "samplewebvaultbucket"
+}
 
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
   }
 }
 
-resource "aws_s3_bucket_object" "index" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-  key    = "index.html"
-  source = "${path.module}/../website/index.html"
-  acl    = "public-read"
-  content_type = "text/html"
-}
+# Add a bucket policy to allow public read access to the website files
+resource "aws_s3_bucket_policy" "website_policy" {
+  bucket = aws_s3_bucket.website_bucket.id
 
-output "website_url" {
-  value = aws_s3_bucket.website_bucket.website_endpoint
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.website_bucket.arn}/*"
+      }
+    ]
+  })
 }
