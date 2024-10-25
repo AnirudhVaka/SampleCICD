e410921bc02292bbb23ecc59d99e1159e477f655
Anirudh\anirudh <anirudh@snovasys.com>
2024-10-25 23:35:23 +0530

    Test Case Updated

--- test/s3_terratest_test.go
+++ test/s3_terratest_test.go
@@ -1,8 +1,6 @@
 package test
 
 import (
-	"fmt"
-	"net/http"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -12,13 +10,11 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestS3WebsiteHttpAccess(t *testing.T) {
+func TestS3BucketExistence(t *testing.T) {
 	t.Parallel()
 
 	awsRegion := "ap-south-1"
-	bucketName := "samplewebsitebucket"
 
-	// Terraform options for applying configuration
 	terraformOptions := &terraform.Options{
 		TerraformDir: "../terraform",
 		EnvVars: map[string]string{
@@ -26,32 +22,23 @@ func TestS3WebsiteHttpAccess(t *testing.T) {
 		},
 	}
 
-	// Ensure Terraform destroy is run at the end of the test
 	defer terraform.Destroy(t, terraformOptions)
 
 	// Apply Terraform code
 	terraform.InitAndApply(t, terraformOptions)
 
-	// Set up AWS session
+	// Check if the S3 bucket exists using AWS SDK
+	bucketName := "samplewebsitebucket"
 	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String(awsRegion),
-	})
+		Region: aws.String(awsRegion)},
+	)
 	assert.NoError(t, err)
 
-	// Verify the bucket is configured as a website
 	s3Client := s3.New(sess)
-	_, err = s3Client.GetBucketWebsite(&s3.GetBucketWebsiteInput{
+	_, err = s3Client.HeadBucket(&s3.HeadBucketInput{
 		Bucket: aws.String(bucketName),
 	})
-	assert.NoError(t, err, "The S3 bucket should be configured as a website")
 
-	// Construct the website URL
-	websiteURL := fmt.Sprintf("http://%s.s3-website-%s.amazonaws.com", bucketName, awsRegion)
-
-	// Make an HTTP GET request to the website URL
-	resp, err := http.Get(websiteURL)
-	assert.NoError(t, err, "HTTP request to the S3 website should not error")
-
-	// Check that the response status is 200 OK
-	assert.Equal(t, http.StatusOK, resp.StatusCode, "Expected HTTP status 200 OK for the S3 website")
+	bucketExists := err == nil
+	assert.True(t, bucketExists, "The S3 bucket should exist")
 }
