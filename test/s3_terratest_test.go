61241330813877fc0ce690c9ba6fb925187aa3bb
Anirudh\anirudh <anirudh@snovasys.com>
2024-10-25 18:58:52 +0530

    updated file

--- test/s3_terratest_test.go
+++ test/s3_terratest_test.go
@@ -1,7 +1,6 @@
 package test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/aws"
@@ -9,40 +8,57 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestS3Website(t *testing.T) {
+func cleanup(t *testing.T, terraformOptions *terraform.Options) {
+	terraform.Destroy(t, terraformOptions)
+}
+
+// Test if the S3 bucket and its website configuration are created and accessible
+func TestS3WebsiteBucket(t *testing.T) {
 	t.Parallel()
+	region := "ap-south-1"              // Update to your region if different
+	bucketName := "samplewebsitebucket" // Matches bucket name in main.tf
 
-	// Specify the path to your Terraform code
-	tfOptions := &terraform.Options{
-		TerraformDir: "../terraform", // Update this path accordingly
-
-		// Variables to pass to our Terraform code using -var options
+	// Configure Terraform options
+	terraformOptions := &terraform.Options{
+		TerraformDir: "../..", // Path to the directory where main.tf is located
 		Vars: map[string]interface{}{
-			"bucket_name": "samplewebsitebucket",
+			"bucket": bucketName,
 		},
-
-		// Disable colors in Terraform commands so its easier to parse stdout/stderr
-		NoColor: true,
 	}
 
-	// Clean up resources with 'terraform destroy' at the end of the test
-	defer terraform.Destroy(t, tfOptions)
+	// Cleanup resources with Terraform destroy at the end
+	defer cleanup(t, terraformOptions)
 
-	// This will run 'terraform init' and 'terraform apply'. Fail the test if there are any errors.
-	initAndApply := terraform.InitAndApply(t, tfOptions)
+	// Run `terraform init` and `terraform apply` and fail if any errors occur
+	terraform.InitAndApply(t, terraformOptions)
 
-	// Check that the bucket was created or exists
-	bucketExists := aws.S3BucketExists(t, "samplewebsitebucket")
+	// Test 1: Check if the bucket was created
+	actualBucketID := terraform.Output(t, terraformOptions, "bucket_id")
+	assert.Equal(t, bucketName, actualBucketID)
+
+	// Test 2: Verify bucket exists in AWS
+	bucketExists := aws.DoesS3BucketExist(t, region, bucketName)
 	assert.True(t, bucketExists, "Expected S3 bucket to exist")
 
-	if initAndApply {
-		// Optionally check if the website configuration is set up correctly
-		bucketWebsiteURL := terraform.Output(t, tfOptions, "website_url")
+	// Test 3: Check website configuration - index document
+	websiteConfig := aws.GetS3BucketWebsiteConfiguration(t, region, bucketName)
+	assert.Equal(t, "index.html", websiteConfig.IndexDocumentSuffix)
+	assert.Equal(t, "error.html", websiteConfig.ErrorDocumentKey)
 
-		assert.NotEmpty(t, bucketWebsiteURL, "Expected website URL to be set")
+	// Test 4: Check bucket policy - public access allowed
+	bucketPolicy := aws.GetS3BucketPolicy(t, region, bucketName)
+	assert.Contains(t, bucketPolicy, "s3:GetObject")
+	assert.Contains(t, bucketPolicy, "arn:aws:s3:::"+bucketName+"/*")
 
-		fmt.Printf("Website URL: %s\n", bucketWebsiteURL)
+	// Test 5: Check that the public access block is set correctly
+	publicAccessBlock := aws.GetS3BucketPublicAccessBlock(t, region, bucketName)
+	assert.False(t, publicAccessBlock.BlockPublicAcls)
+	assert.False(t, publicAccessBlock.BlockPublicPolicy)
+	assert.False(t, publicAccessBlock.IgnorePublicAcls)
+	assert.False(t, publicAccessBlock.RestrictPublicBuckets)
 
-		// Here you could add more assertions to check if the website is accessible
-	}
+	// Test 6: Confirm the website endpoint output
+	expectedWebsiteURL := "http://" + bucketName + ".s3-website-" + region + ".amazonaws.com"
+	websiteURL := terraform.Output(t, terraformOptions, "website_url")
+	assert.Equal(t, expectedWebsiteURL, websiteURL)
 }
