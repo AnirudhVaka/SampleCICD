ffd2b0124b2e40abcf44dfde1aabb33ad3f5b4b1
Anirudh\anirudh <anirudh@snovasys.com>
2024-10-25 18:02:18 +0530

    updated test case

--- test/s3_terratest_test.go
+++ test/s3_terratest_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/aws"
@@ -8,20 +9,40 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestS3BucketVersioning(t *testing.T) {
-	// Specify the Terraform options
-	terraformOptions := &terraform.Options{
-		// Path to the Terraform code
-		TerraformDir: "../terraform",
+func TestS3Website(t *testing.T) {
+	t.Parallel()
+
+	// Specify the path to your Terraform code
+	tfOptions := &terraform.Options{
+		TerraformDir: "../terraform", // Update this path accordingly
+
+		// Variables to pass to our Terraform code using -var options
+		Vars: map[string]interface{}{
+			"bucket_name": "samplewebsitebucket",
+		},
+
+		// Disable colors in Terraform commands so its easier to parse stdout/stderr
+		NoColor: true,
 	}
 
-	// Apply and defer cleanup
-	defer terraform.Destroy(t, terraformOptions)
-	terraform.InitAndApply(t, terraformOptions)
+	// Clean up resources with 'terraform destroy' at the end of the test
+	defer terraform.Destroy(t, tfOptions)
 
-	// Check if the S3 bucket exists and has versioning enabled
-	bucketID := terraform.Output(t, terraformOptions, "bucket_id")
-	region := "ap-south-1"
-	isVersioningEnabled := aws.IsS3BucketVersioningEnabled(t, region, bucketID)
-	assert.True(t, isVersioningEnabled, "Bucket versioning should be enabled.")
+	// This will run 'terraform init' and 'terraform apply'. Fail the test if there are any errors.
+	initAndApply := terraform.InitAndApply(t, tfOptions)
+
+	// Check that the bucket was created or exists
+	bucketExists := aws.S3BucketExists(t, "samplewebsitebucket")
+	assert.True(t, bucketExists, "Expected S3 bucket to exist")
+
+	if initAndApply {
+		// Optionally check if the website configuration is set up correctly
+		bucketWebsiteURL := terraform.Output(t, tfOptions, "website_url")
+
+		assert.NotEmpty(t, bucketWebsiteURL, "Expected website URL to be set")
+
+		fmt.Printf("Website URL: %s\n", bucketWebsiteURL)
+
+		// Here you could add more assertions to check if the website is accessible
+	}
 }
