16f557525739be62ddd17b3fc78c90015d3183d9
Anirudh\anirudh <anirudh@snovasys.com>
2024-10-25 12:00:01 +0530

    update main ter file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,14 @@
-# Provider configuration for AWS
-provider "aws" {
-  region = "ap-south-1"
-}
+# Conditional creation of S3 bucket as described earlier
 
 # Data source to check if the bucket exists
 data "aws_s3_bucket" "existing_bucket" {
-  bucket = "samplewebsitebucket"
-  # Ignore errors if the bucket doesn't exist, which will allow conditional creation
+  bucket = "samplewebvaultbucket"
   count  = length(try(aws_s3_bucket.website_bucket.id, [])) == 0 ? 0 : 1
 }
 
 # Conditionally create the bucket if it does not already exist
 resource "aws_s3_bucket" "website_bucket" {
-  bucket = "samplewebsitebucket"
-  
-  # Only create the bucket if the data source didn't find an existing one
+  bucket = "samplewebvaultbucket"
   count = length(data.aws_s3_bucket.existing_bucket) == 0 ? 1 : 0
 
   website {
@@ -23,7 +17,7 @@ resource "aws_s3_bucket" "website_bucket" {
   }
 }
 
-# Bucket policy to allow public read access
+# Bucket policy for public access
 resource "aws_s3_bucket_policy" "website_policy" {
   bucket = aws_s3_bucket.website_bucket[0].id
 
@@ -38,14 +32,10 @@ resource "aws_s3_bucket_policy" "website_policy" {
       }
     ]
   })
-  
-  # Only apply the policy if the bucket was created
   count = length(data.aws_s3_bucket.existing_bucket) == 0 ? 1 : 0
 }
 
 output "website_url" {
   value = aws_s3_bucket.website_bucket[0].website_endpoint
-  description = "The URL of the static website hosted on S3"
-  # Only output if the bucket was created
   depends_on = [aws_s3_bucket.website_bucket]
 }
