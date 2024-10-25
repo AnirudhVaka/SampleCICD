ad69a76d7a605cb55776baee793870668b65866a
Anirudh\anirudh <anirudh@snovasys.com>
2024-10-25 17:06:52 +0530

    updated tests

--- test/s3_terratest_test.go
+++ test/s3_terratest_test.go
@@ -16,14 +16,6 @@ func TestS3BucketWebsite(t *testing.T) {
     terraformOptions := &terraform.Options{
         // Path to the Terraform code that provisions the S3 bucket
         TerraformDir: "../terraform",
-
-        // Variables to pass to our Terraform code (if any)
-        // Variables: map[string]interface{}{
-        //     "example_var": "value",
-        // },
-
-        // Variables to specify if you want to capture output
-        // Output: true,
     }
 
     // Clean up resources after tests
@@ -41,15 +33,13 @@ func TestS3BucketWebsite(t *testing.T) {
     // Check if the website URL is not empty
     assert.NotEmpty(t, websiteURL, "Website URL should not be empty")
 
-    // Ensure the website URL is a valid format (optional)
+    // Ensure the website URL is a valid format
     assert.Regexp(t, `^https?://`, websiteURL, "Website URL should start with http:// or https://")
 
     // Test the website endpoint using HTTP helper if a website URL is provided
-    if websiteURL != "" {
-        maxRetries := 10
-        timeBetweenRetries := 10 * time.Second
+    maxRetries := 10
+    timeBetweenRetries := 10 * time.Second
 
-        // Perform an HTTP GET request with retries
-        http_helper.HttpGetWithRetry(t, websiteURL, nil, 200, "", maxRetries, timeBetweenRetries)
-    }
+    // Perform an HTTP GET request with retries
+    http_helper.HttpGetWithRetry(t, websiteURL, nil, 200, "", maxRetries, timeBetweenRetries)
 }
