a97b8fadd1382b7ec48dc0cffd46c3c7d0e3e905
Anirudh\anirudh <anirudh@snovasys.com>
2024-10-25 13:10:27 +0530

    website count change

--- terraform/main.tf
+++ terraform/main.tf
@@ -56,8 +56,11 @@ resource "aws_s3_bucket_policy" "website_policy" {
   })
 }
 
-# Output the S3 website URL, depending on whether the bucket is pre-existing or newly created
+# Step 6: Output the S3 website URL
 output "website_url" {
-  value       = coalesce(data.aws_s3_bucket.existing.website_endpoint, try(aws_s3_bucket_website_configuration.website.website_endpoint, null))
+  value       = coalesce(
+    data.aws_s3_bucket.existing.website_endpoint,
+    try(aws_s3_bucket_website_configuration.website[0].website_endpoint, null)
+  )
   description = "The URL of the static website hosted on S3"
 }
