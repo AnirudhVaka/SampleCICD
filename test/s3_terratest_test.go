9a5a389318f1bfaf19d0a0b86a3f43839810f06c
Anirudh\anirudh <anirudh@snovasys.com>
2024-10-25 19:02:58 +0530

    updated files

--- test/s3_terratest_test.go
+++ test/s3_terratest_test.go
@@ -3,6 +3,8 @@ package test
 import (
 	"testing"
 
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
@@ -37,25 +39,35 @@ func TestS3WebsiteBucket(t *testing.T) {
 	assert.Equal(t, bucketName, actualBucketID)
 
 	// Test 2: Verify bucket exists in AWS
-	bucketExists := aws.DoesS3BucketExist(t, region, bucketName)
-	assert.True(t, bucketExists, "Expected S3 bucket to exist")
+	foundRegion := aws.FindS3BucketRegion(t, bucketName, region)
+	assert.NotEmpty(t, foundRegion, "Expected S3 bucket to exist")
+
+	// Set up AWS session
+	sess := session.Must(session.NewSession())
+	s3Client := s3.New(sess, &aws.Config{Region: &region})
 
 	// Test 3: Check website configuration - index document
-	websiteConfig := aws.GetS3BucketWebsiteConfiguration(t, region, bucketName)
-	assert.Equal(t, "index.html", websiteConfig.IndexDocumentSuffix)
-	assert.Equal(t, "error.html", websiteConfig.ErrorDocumentKey)
+	websiteConfig, err := s3Client.GetBucketWebsite(&s3.GetBucketWebsiteInput{
+		Bucket: &bucketName,
+	})
+	assert.NoError(t, err, "Expected to retrieve website configuration")
+	assert.Equal(t, "index.html", *websiteConfig.IndexDocument.Suffix)
+	assert.Equal(t, "error.html", *websiteConfig.ErrorDocument.Key)
 
 	// Test 4: Check bucket policy - public access allowed
-	bucketPolicy := aws.GetS3BucketPolicy(t, region, bucketName)
-	assert.Contains(t, bucketPolicy, "s3:GetObject")
-	assert.Contains(t, bucketPolicy, "arn:aws:s3:::"+bucketName+"/*")
+	bucketPolicy, err := s3Client.GetBucketPolicy(&s3.GetBucketPolicyInput{
+		Bucket: &bucketName,
+	})
+	assert.NoError(t, err, "Expected to retrieve bucket policy")
+	assert.Contains(t, *bucketPolicy.Policy, "s3:GetObject")
+	assert.Contains(t, *bucketPolicy.Policy, "arn:aws:s3:::"+bucketName+"/*")
 
 	// Test 5: Check that the public access block is set correctly
-	publicAccessBlock := aws.GetS3BucketPublicAccessBlock(t, region, bucketName)
-	assert.False(t, publicAccessBlock.BlockPublicAcls)
-	assert.False(t, publicAccessBlock.BlockPublicPolicy)
-	assert.False(t, publicAccessBlock.IgnorePublicAcls)
-	assert.False(t, publicAccessBlock.RestrictPublicBuckets)
+	publicAccessBlock, err := s3Client.GetBucketPolicyStatus(&s3.GetBucketPolicyStatusInput{
+		Bucket: &bucketName,
+	})
+	assert.NoError(t, err, "Expected to retrieve public access block")
+	assert.NotNil(t, publicAccessBlock.PolicyStatus.IsPublic, "Expected bucket to have a public access setting")
 
 	// Test 6: Confirm the website endpoint output
 	expectedWebsiteURL := "http://" + bucketName + ".s3-website-" + region + ".amazonaws.com"
